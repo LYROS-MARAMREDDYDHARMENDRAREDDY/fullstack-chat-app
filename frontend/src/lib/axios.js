e1a2733e10dc31688e067eed7439afeeacca8e77
LYROS-MARAMREDDYDHARMENDRAREDDY <dharmendramaram@lyrostech.com>
2025-04-22 10:35:43 +0530

    commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-77bi.onrender.com/api" : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
