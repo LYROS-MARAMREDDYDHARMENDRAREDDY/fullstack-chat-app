de8fef54a088e604b49afe8845a75912403cca7d
LYROS-MARAMREDDYDHARMENDRAREDDY <dharmendramaram@lyrostech.com>
2025-04-25 12:34:44 +0530

    commit

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://fullstack-chat-app-pi.vercel.app", 
+      "https://fullstack-chat-app-coral.vercel.app", 
     ],
     credentials: true,
   },
